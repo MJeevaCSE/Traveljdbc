65d3c3053a7143606c8d75059d8dccf3b01c4673
Jeeva <144088242+MJeevaCSE@users.noreply.github.com>
2024-08-19 19:51:38 +0530

    Add files via upload

---
+++ traveller/src/model/Product.java
@@ -0,0 +1,54 @@
+package model;
+
+public class Product {
+
+
+
+	private int ID;
+	private String UserName;
+	private String Bookingdate;
+
+	private String Travelplace;
+	private String Checkin;
+	private String Checkout;
+	public int getId() {
+		return ID;
+	}
+	public void setId(int ID) {
+		this.ID = ID;
+	}
+	public String getName() {
+		return UserName;
+	}
+	public void setName(String name) {
+		UserName = name;
+	}
+	public String getBookingdate() {
+		return Bookingdate;
+	}
+	public void setBookingdate(String bookingdate) {
+		Bookingdate = bookingdate;
+	}
+	public String getTravelplace() {
+		return Travelplace;
+	}
+	public void setTravelplace(String travelplace) {
+		Travelplace = travelplace;
+	}
+	public String getCheckin() {
+		return Checkin;
+	}
+	public void setCheckin(String checkin) {
+		Checkin = checkin;
+	}
+	public String getCheckout() {
+		return Checkout;
+	}
+	public void setCheckout(String checkout) {
+		Checkout = checkout;
+	}
+	
+	
+
+	
+}
